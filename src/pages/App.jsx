f2a873f1b69d500fc6eae5f44061c1e03eed85c2
43215-Anuj <43952928+43215-Anuj@users.noreply.github.com>
2021-08-01 14:17:38 +0530

    privacy policy

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,76 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import {  BrowserRouter, Route, Switch } from 'react-router-dom';
-import './App.scss';
-import Layout from '../hoc/Layout';
-import Home from './Home/Home';
-import About from './About/About';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.scss";
+import Layout from "../hoc/Layout";
+import Home from "./Home/Home";
+import About from "./About/About";
 //Team
-import Founders from './Team/Founder/Founder';
-import Exe from './Team/Executive/Executives';
-import Advisory from './Team/Advisory/Members';
-import VolunteersName from './Volunteers/List/Volunteers'
+import Founders from "./Team/Founder/Founder";
+import Exe from "./Team/Executive/Executives";
+import Advisory from "./Team/Advisory/Members";
+import VolunteersName from "./Volunteers/List/Volunteers";
 
-import LifeMembers from './Members/Life/Life';
-import AssociateMembers from './Members/Associate/Associate';
+import LifeMembers from "./Members/Life/Life";
+import AssociateMembers from "./Members/Associate/Associate";
 
-import Drawing from './OurEvents/Drawing';
-import Collab from './Collab/Collaborators';
-import MediaFeatured from './Media/Media';
-import Donate from './Donate/Donate';
-import Research from './Newsletter/Research';
-import JoinVolunteer from '../components/JoinMember/JoinVolunteer';
-import JoinMember from '../components/JoinMember/JoinMember';
+import Drawing from "./OurEvents/Drawing";
+import Collab from "./Collab/Collaborators";
+import MediaFeatured from "./Media/Media";
+import Donate from "./Donate/Donate";
+import Research from "./Newsletter/Research";
+import JoinVolunteer from "../components/JoinMember/JoinVolunteer";
+import JoinMember from "../components/JoinMember/JoinMember";
 
-
-import jsonData from '../data/data.json';
-import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import Privacy from "../pages/Privacy/Privacy";
+import jsonData from "../data/data.json";
+import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 
 function App() {
-    useEffect(() => {
-      window.scrollTo({
-          top: 0, 
-          behavior: 'smooth'
-        });
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
   }, []);
 
   const [visible, setVisible] = useState(false);
   const toggleVisible = () => {
-      const scrolled = document.documentElement.scrollTop;
-      if (scrolled > 300){
-        setVisible(true)
-      } 
-      else if (scrolled <= 300){
-        setVisible(false)
-      }
-    };
-
-  const scrollToTop = () =>{
-      window.scrollTo({
-        top: 0, 
-        behavior: 'smooth'
-      });
+    const scrolled = document.documentElement.scrollTop;
+    if (scrolled > 300) {
+      setVisible(true);
+    } else if (scrolled <= 300) {
+      setVisible(false);
+    }
   };
 
-  window.addEventListener('scroll', toggleVisible);
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  window.addEventListener("scroll", toggleVisible);
 
   return (
     <div className="App">
       <BrowserRouter>
         <Layout>
-          <ExpandLessIcon className="scroll-top-btn" onClick={scrollToTop} 
-          style={{display: visible ? 'inline' : 'none'}} />
+          <ExpandLessIcon
+            className="scroll-top-btn"
+            onClick={scrollToTop}
+            style={{ display: visible ? "inline" : "none" }}
+          />
           <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/home" component={Home} />
             <Route path="/about" component={About} />
-            
-            <Route path="/team/founder" render={() => <Founders data={jsonData.Founders} /> } />
-            <Route path="/team/executives" render={() => <Exe data={jsonData.Executives} /> } />
-            <Route path="/team/advisory" render={() => <Advisory data={jsonData.Advisory} /> } />
-    
-            <Route path="/volunteers" render={()=> <VolunteersName 
-                  Volunteers={jsonData.Volunteers} 
-                  WebVolunteers={jsonData.WebVolunteers} /> } />
+
+            <Route
+              path="/team/founder"
+              render={() => <Founders data={jsonData.Founders} />}
+            />
+            <Route
+              path="/team/executives"
+              render={() => <Exe data={jsonData.Executives} />}
+            />
+            <Route
+              path="/team/advisory"
+              render={() => <Advisory data={jsonData.Advisory} />}
+            />
+
+            <Route
+              path="/volunteers"
+              render={() => (
+                <VolunteersName
+                  Volunteers={jsonData.Volunteers}
+                  WebVolunteers={jsonData.WebVolunteers}
+                />
+              )}
+            />
 
             <Route path="/events/covid_drawing" component={Drawing} />
             <Route path="/media_featured" component={MediaFeatured} />
@@ -79,10 +96,10 @@ function App() {
             <Route path="/collaborators" component={Collab} />
             <Route path="/member_form" component={JoinMember} />
             <Route path="/volunteer_form" component={JoinVolunteer} />
-
+            <Route path="/privacy_policy" component={Privacy} />
             <Route path="/donate" component={Donate} />
             <Route path="/members/life" component={LifeMembers} />
-           </Switch>
+          </Switch>
         </Layout>
       </BrowserRouter>
     </div>
